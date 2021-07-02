cd7c3694be11bb8f8302be7b73c2800090d8e48b
yug90 <78593346+yug90@users.noreply.github.com>
2021-07-02 23:00:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer,rubber;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(1000,300);
+    rubber = new Rubber(700,520,90,30)
+    iron = new Iron(700,250,20,75);
+    stone = new Stone(360,550)
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    iron.display();
+    stone.display();
+ 
+}
